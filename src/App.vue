916c81e8b8283f8e02ac640a7f5243f83a927a4e
Asbert Ma <stayinalive@msn.cn>
2021-04-19 16:47:48 +0800

    remove the component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
     <div class="app">
-        <Bootstrap v-if="$env.target.href"/>
-        <Home v-else/>
+        <Home/>
     </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import Home from './components/Home.vue'
-import Bootstrap from './components/Bootstrap.vue'
 
 @Component({
     components: {
-        Home,
-        Bootstrap,
-    },
+        Home
+    }
 })
 export default class App extends Vue {
 }
