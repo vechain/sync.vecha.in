dde6dc583108cbcd92d404b5883fbdb27c85c437
libotony <libotony@users.noreply.github.com>
2023-06-06 03:34:30 +0000

    deploy: ebf55a0a33ac779e7798d45bfafc4ebb3cb483f8

---
+++ precache-manifest.0feeb034647cabe77421ee73fa9668c0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b5781f926d3dc6fc1c44d13460f54e8",
+    "url": "/img/platforms.2b5781f9.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "555f3f22ed923ddb261ce37737d40304",
+    "url": "/img/dapps.555f3f22.svg"
+  },
+  {
+    "revision": "7220ecf72c302519b045",
+    "url": "/js/chunk-vendors.b60846b5.js"
+  },
+  {
+    "revision": "89e753a9ff6e17dff4ff",
+    "url": "/js/index.25c9ab0f.js"
+  },
+  {
+    "revision": "525a6da9519a3970293a92fdf53449ea",
+    "url": "/img/windows.525a6da9.svg"
+  },
+  {
+    "revision": "04834f6321978195d2fb",
+    "url": "/js/checksum.5e635f55.js"
+  },
+  {
+    "revision": "7bba9545db61ebe5f4f9d754c9b92f84",
+    "url": "/img/screenshot.7bba9545.png"
+  },
+  {
+    "revision": "cf3878b5dcbcc2466f4a143ea7f8e697",
+    "url": "/img/sync2-logo.cf3878b5.svg"
+  },
+  {
+    "revision": "9f2cfbebb6bbe722cc3396501c8ddb61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e74434b98d80e775c78730a2deeba39",
+    "url": "/img/vechain-logo.3e74434b.svg"
+  },
+  {
+    "revision": "89c8b4289eccab7a8a3aaa718aaeaaba",
+    "url": "/img/macOS.89c8b428.svg"
+  },
+  {
+    "revision": "7ee35d1a3b65263c23dd1020d06fbfd0",
+    "url": "/img/linux.7ee35d1a.svg"
+  },
+  {
+    "revision": "eff8b17aeeb5d897879937ed53567223",
+    "url": "/img/github-logo.eff8b17a.svg"
+  },
+  {
+    "revision": "6e7d908389691bb3351b957f2c62c44a",
+    "url": "/img/asset.6e7d9083.svg"
+  },
+  {
+    "revision": "bc76848bd2df360a96dc1dbc845fafc9",
+    "url": "/img/android.bc76848b.svg"
+  },
+  {
+    "revision": "89e753a9ff6e17dff4ff",
+    "url": "/css/index.bfeefaf4.css"
+  },
+  {
+    "revision": "7220ecf72c302519b045",
+    "url": "/css/chunk-vendors.1f1f61a1.css"
+  },
+  {
+    "revision": "a7853ce4595708e284ea2c614c447de4",
+    "url": "/checksum.html"
+  },
+  {
+    "revision": "68b329da9893e34099c7d8ad5cb9c940",
+    "url": "/blocklist.txt"
+  },
+  {
+    "revision": "a295797fcc2a76117e67d8a336d9774e",
+    "url": "/404.html"
+  }
+];
